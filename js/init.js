1ae82a4bf6bfff5aa1d999799be725a2ed8d3b70
estadios41mell <estadios41mell@gmail.com>
2023-04-13 00:08:00 -0500

    cambio

---
+++ js/init.js
@@ -0,0 +1,10 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyB-q-H9kqY58KlUP6YDhpx1DJ9C5LsxVdE",
+    authDomain: "estadios41mell-6b488.firebaseapp.com",
+    projectId: "estadios41mell-6b488",
+    storageBucket: "estadios41mell-6b488.appspot.com",
+    messagingSenderId: "692530541335",
+    appId: "1:692530541335:web:262cf57e11c6c5b0fa7d52",
+    measurementId: "G-QFHTM0YTM2"
+       });
+   
